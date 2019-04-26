2bea51b15995f971aaaaa05b8df7860c02189484
sumit <abey.abraham.628@gmail.com>
2019-04-26 16:04:45 +0530

    chatbot new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,10 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+import { TextToSpeech } from '@ionic-native/text-to-speech';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-
+import { SpeechRecognition } from '@ionic-native/speech-recognition';
 @NgModule({
   declarations: [
     MyApp,
@@ -23,7 +23,9 @@ import { HomePage } from '../pages/home/home';
   ],
   providers: [
     StatusBar,
+    TextToSpeech,
     SplashScreen,
+    SpeechRecognition,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,98 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component , NgZone  } from '@angular/core';
+import { IonicPage, NavController, NavParams , Platform } from 'ionic-angular';
+import { TextToSpeech } from '@ionic-native/text-to-speech';
+import { SpeechRecognition } from '@ionic-native/speech-recognition';
 
+declare var ApiAIPromises: any;
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
+
+/**
+ * Generated class for the ChatbotPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+
+
+
 export class HomePage {
+  question:any
+   constructor(private speechRecognition: SpeechRecognition,private tts: TextToSpeech,public navCtrl: NavController, public navParams: NavParams , public platform: Platform, public ngZone: NgZone) {
+     platform.ready().then(() => {
+       ApiAIPromises.init({
+         clientAccessToken: "your access token "
+       }).then(result => console.log(result));
+     });
+     
+// Check feature available
+this.speechRecognition.isRecognitionAvailable()
+.then((available: boolean) => console.log(available))
 
-  constructor(public navCtrl: NavController) {
 
-  }
+// Get the list of supported languages
+this.speechRecognition.getSupportedLanguages()
+.then(
+  (languages: Array<string>) => console.log(languages),
+  (error) => console.log(error)
+)
 
-}
+// Check permission
+this.speechRecognition.hasPermission()
+.then((hasPermission: boolean) => console.log("idliiiiii",hasPermission))
+
+// Request permissions
+this.speechRecognition.requestPermission()
+.then(
+  () => console.log('Granted'),
+  () => console.log('Denied')
+)
+    
+   }
+ 
+   ionViewDidLeave() {
+     this.navCtrl.popToRoot();
+   }
+   matcheesa=[]
+ chat=[]
+   questions=[]
+   answer:any=[];
+    ask(question) {
+     this.chat.push({'question':question})
+     
+   
+     
+     ApiAIPromises.requestText({
+       query: question
+     }).then(({result: {fulfillment: {speech}}}) => {
+        this.ngZone.run(()=> {
+          this.chat.push({'answer':speech});
+          this.tts.speak(`${speech}`)
+        });
+ 
+     })
+     this.question="";
+   }
+   async startListen(){
+     
+    await  this.speechRecognition.startListening()
+    .subscribe(
+      (matches: Array<string>) =>  this.ask(matches[0]),
+    )
+  
+   }
+   stopListen(){
+    this.speechRecognition.stopListening()
+   }
+ 
+ 
+
+ 
+ 
+ 
+ 
+ }
